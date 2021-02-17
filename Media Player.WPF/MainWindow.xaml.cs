cc0f75bdcaecfcdd74e7b68f1cc63642ff075af1
Thomas <thomaspmar@protonmail.com>
2021-02-16 21:52:08 -0800

    Added JsonSerializer. Started setting up MainWindow

--- Media Player.WPF/MainWindow.xaml.cs
+++ Media Player.WPF/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using Media_Player.Lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Media_Player.WPF
 {
@@ -20,9 +13,38 @@ namespace Media_Player.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow instance;
+
         public MainWindow()
         {
             InitializeComponent();
+            instance = this;
+            Logger.MessageLogged += Logger_MessageLogged;
+        }
+
+
+        public static void RunOnUIThread(Action action) => instance.Dispatcher.Invoke(action);
+        private void Window_ContentRendered(object sender, EventArgs e) => FinishedLoading();
+        private void Window_Closed(object sender, EventArgs e) => Application.Current.Shutdown();
+
+
+        private void Logger_MessageLogged(object sender, Logger.LogEvents e)
+        {
+            if (e.LogType == Logger.LogType.MessageBox)
+                MessageBox.Show(e.Message);
+
+            RunOnUIThread(() => 
+            {
+                if (e.LogType == Logger.LogType.Console)
+                {
+
+                }
+            });
+        }
+
+        private void FinishedLoading()
+        {
+
         }
     }
 }
