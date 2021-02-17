54fc0b9d75311da00b960307772e14ac51e8769e
Thomas <thomaspmar@protonmail.com>
2021-02-16 21:56:39 -0800

    Added some extensions

---
+++ Media Player.Lib/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Media_Player.Extensions
+{
+    public static class AssemblyExt
+    {
+        public static string GetDirectory(this Assembly assembly)
+        {
+            return new FileInfo(assembly.Location).Directory.FullName;
+        }
+    }
+}
---
+++ Media Player.WPF/Extensions/RichTextboxExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Media_Player.Extensions
+{
+    public static class RichTextBoxExt
+    {
+        public static bool IsEmpty(this RichTextBox rtb) => string.IsNullOrEmpty(rtb.GetText().Trim());
+
+        public static string GetText(this RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+            return textRange.Text;
+        }
+
+        public static void SetText(this RichTextBox rtb, string text)
+        {
+            TextRange textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+            textRange.Text = text;
+        }
+    }
+}
