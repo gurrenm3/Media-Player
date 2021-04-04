5ef7cc2a685c3cae4d8cc3028a82e113bcbfe364
Thomas <thomaspmar@protonmail.com>
2021-04-03 22:25:18 -0700

    Implemented play/pause button

--- Media Player.WPF/UserControls/VideoPlayer.xaml.cs
+++ Media Player.WPF/UserControls/VideoPlayer.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Media_Player.Extensions;
 
 namespace Media_Player.WPF.UserControls
 {
@@ -20,9 +22,38 @@ namespace Media_Player.WPF.UserControls
     /// </summary>
     public partial class VideoPlayer : UserControl
     {
+        bool waiting = false;
         public VideoPlayer()
         {
             InitializeComponent();
+            ControlsPanel.Visibility = Visibility.Hidden;
+        }
+
+        private async void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (waiting)
+                return;
+
+            waiting = true;
+            ControlsPanel.Visibility = Visibility.Visible;
+            await Task.Factory.StartNew(() => Thread.Sleep(5000));
+            ControlsPanel.Visibility = Visibility.Hidden;
+            waiting = false;
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Player.LoadedBehavior = Player.IsPlaying() ? MediaState.Pause : MediaState.Play;
+        }
+
+        private void ForwardTen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RewindTen_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
