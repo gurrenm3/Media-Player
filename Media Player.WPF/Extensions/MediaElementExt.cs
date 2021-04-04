ab5ea0a5872bd78fea91c395ea5304e92ec0717c
Thomas <thomaspmar@protonmail.com>
2021-04-03 22:24:26 -0700

    Created a few MediaElement extensions

---
+++ Media Player.WPF/Extensions/MediaElementExt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Media_Player.Extensions
+{
+    public static class MediaElementExt
+    {
+        public static bool IsPlaying(this MediaElement mediaElement)
+        {
+            return mediaElement.LoadedBehavior == MediaState.Play;
+        }
+
+        public static bool IsPaused(this MediaElement mediaElement)
+        {
+            return mediaElement.LoadedBehavior == MediaState.Pause;
+        }
+
+        public static bool IsStopped(this MediaElement mediaElement)
+        {
+            return mediaElement.LoadedBehavior == MediaState.Stop;
+        }
+    }
+}
