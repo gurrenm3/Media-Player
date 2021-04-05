ff07e8481874d81f2ec82f8d5dfe31469b665d28
Thomas <thomaspmar@protonmail.com>
2021-04-04 23:22:08 -0700

    Added Extensions for Images, RichTextBoxes, and MediaElements

---
+++ Media_Player.WPF/Extensions/ImageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Bitmap = System.Drawing.Bitmap;
+
+namespace Media_Player.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static void SetSource(this Image image, string filePath)
+        {
+            image.SetSource(new BitmapImage(new Uri(filePath)));
+        }
+
+        public static void SetSource(this Image image, BitmapImage bitmapImage)
+        {
+            image.Source = bitmapImage;
+        }
+
+        public static void SetSource(this Image image, Bitmap bitmap)
+        {
+            image.SetSource(bitmap.ToBitmapImage());
+        }
+    }
+}
