27867cd22df22deb25bdea4fd0245a81d2108828
Thomas <thomaspmar@protonmail.com>
2021-04-04 23:18:22 -0700

    Created Bitmap and BitmapImage extensions

---
+++ Media_Player.Shared/Extensions/BitmapImageExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Media_Player.Extensions
+{
+    public static class BitmapImageExtensions
+    {
+        public static Bitmap ToBitmap(this BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+    }
+}
